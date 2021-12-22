a0632cbbf774ab719c3bf7cefae93e61c851fdea
VoNo <xkaioken9x@gmail.com>
2021-12-22 14:13:03 +0700

    update bg-color edit input

--- process_signIn.php
+++ process_signIn.php
@@ -9,30 +9,32 @@
             die("Connect failure");
         }
 
-        $sql = "SELECT * FROM db_user where email_user = ?";
+        $sql = "SELECT * FROM db_nguoidung WHERE email_user = ? OR name_user = ?";
         $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, "s", $email);
+        $user = $email;
+        mysqli_stmt_bind_param($stmt, "ss", $email, $user);
+        $stmt_02 = mysqli_stmt_excute($stmt);
+        echo "Failure";
 
-        if(mysqli_stmt_excute($stmt)) {
-            echo "success";
-            // mysqli_stmt_bind_result($stmt, $ma_user, $ten_user,$email_user, $pass_user);
+        // if(mysqli_stmt_excute($stmt)) {
+        //     mysqli_stmt_bind_result($stmt, $ma_user, $ten_user,$email_user, $pass_user);
             
-            // if(mysqli_stmt_fetch($stmt)) {
-            //     if(password_verify($pass, $pass_user)) {
-            //         // $_SESSION['isLoginOk'] = $email;
-            //         // header("location: signUp.php");
-            //     } else {
-            //         $error = "Bạn đăng nhập sai or mật khẩu không chính xác!";
-            //         header("location: signIn.php?error=$error");
-            //     }
-            // } else {
-            //     $error = "Bạn đăng nhập sai or mật khẩu không chính xác!";
-            //     header("location: signIn.php?error=$error");
-            // }
-        } else{
-            $error = "Bạn nhập thông tin Email hoặc mật khẩu chưa chính xác";
-            header("location: login.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
-        }
+        //     if(mysqli_stmt_fetch($stmt)) {
+        //         if(password_verify($pass, $pass_user)) {
+        //             // $_SESSION['isLoginOk'] = $email;
+        //             // header("location: signUp.php");
+        //         } else {
+        //             $error = "Bạn đăng nhập sai or mật khẩu không chính xác!";
+        //             header("location: signIn.php?error=$error");
+        //         }
+        //     } else {
+        //         $error = "Bạn đăng nhập sai or mật khẩu không chính xác!";
+        //         header("location: signIn.php?error=$error");
+        //     }
+        // } else{
+        //     $error = "Bạn nhập thông tin Email hoặc mật khẩu chưa chính xác";
+        //     header("location: login.php?error=$error"); //Chuyển hướng, hiển thị thông báo lỗi
+        // }
     } else {
         header("location: signIn.php");
     }
