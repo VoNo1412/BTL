b18cb8f1485e6bccf32846c57caeabc54d4eeaa2
VoNo <xkaioken9x@gmail.com>
2021-12-23 09:45:53 +0700

    (23/12 9:44 a.m) add function handlyPost and add table db_comments in mySQL

--- JS/login_user.js
+++ JS/login_user.js
@@ -1,14 +1,29 @@
 const contaienrFluid = document.querySelector(".container-fluid");
 const creatPost = document.querySelector(".create-post");
 const overflow = document.querySelector(".overflow");
+const txtContent = document.querySelector("#txtContent");
+const btnPost = document.querySelector(".btn-post");
+
 
 contaienrFluid.addEventListener("click", (e) => {
-    console.log(e.target.parentElement)
-    if(e.target.tagName === "INPUT") {
+    handlyStatus(e);
+    handlyPost();
+});
+
+function handlyStatus(e) {
+    if(e.target.classList.contains("status")) {
         creatPost.style.display = "block";
         overflow.style.display = "block";
     } else if(e.target.className == "overflow" || e.target.parentElement.classList.contains("Close")){
         creatPost.style.display = "none";
         overflow.style.display = "none";
     }
-})
+}
+
+function handlyPost() {
+    txtContent.addEventListener("keydown", function(e) {
+        if(e.key !== "") {
+            btnPost.classList.add("bg-primary", "fw-bold", "text-white");
+        }
+    })
+}
