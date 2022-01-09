6cff7534d48568ed2feea32c0a634316f4c0abea
VoNo <xkaioken9x@gmail.com>
2022-01-09 09:31:24 +0700

    update login.php

--- login_mynetwork.php
+++ login_mynetwork.php
@@ -1,3 +1,13 @@
+<?php 
+   session_start();
+
+   if(!isset($_SESSION['fullName'])) {
+       header("location: signIn.php");
+   }
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
--- profile.php
+++ profile.php
@@ -1,3 +1,12 @@
+<?php 
+   session_start();
+
+   if(!isset($_SESSION['fullName'])) {
+       header("location: signIn.php");
+   }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
